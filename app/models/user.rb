cd87c28b2cf45ea113cc7d587eea360d3898c07c
agarun <19801205+agarun@users.noreply.github.com>
2017-12-25 16:26:44 -0500

    Add User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,45 @@
 class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  def self.find_by_credentials(username, plaintext_password)
+    user = User.find_by(username: username)
+    user if user && user.valid_password?(plaintext_password)
+  end
+
+  def password
+    @password
+  end
+
+  def password=(plaintext_password)
+    @password = plaintext_password
+    self.password_digest = BCrypt::Password.create(plaintext_password)
+  end
+
+  def valid_password?(plaintext_password)
+    BCrypt::Password
+      .new(password_digest)
+      .is_password?(plaintext_password)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save! && self.session_token
+  end
+
+  after_initialize :ensure_unique_session_token
+
+  private
+
+  def ensure_unique_session_token
+    loop do
+      self.session_token ||= generate_session_token
+      break if User.find_by(session_token: session_token).nil?
+    end
+  end
+
+  def generate_session_token
+    SecureRandom.urlsafe_base64
+  end
 end
