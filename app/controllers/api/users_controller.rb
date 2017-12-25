690ea3c08ebf013818f06cde483739591727d79b
agarun <19801205+agarun@users.noreply.github.com>
2017-12-25 16:38:45 -0500

    Add Users controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::UsersController < ApplicationController
+  def create
+    @new_user = User.new(user_params)
+
+    if @new_user.save
+      log_in(@new_user)
+      render :show
+    else
+      render json: @new_user.errors.full_messages,
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get '/', to: 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-
+    resources :users, only: :create
   end
 end
