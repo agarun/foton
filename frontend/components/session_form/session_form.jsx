250c20ea6edee9ac0c69f5a6b46d4a02912b8f55
agarun <19801205+agarun@users.noreply.github.com>
2017-12-26 14:27:53 -0500

    Retrieve `formType` from `path-to-regexp` keyword

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SessionForm extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
+
+    const newUser = Object.assign({}, this.state);
+    this.props.processForm(newUser);
   }
 
   handleChange(field) {
@@ -21,28 +31,39 @@ class SessionForm extends React.Component {
 
   render() {
     const formType = this.props.formType;
-
+    const errors = this.props.errors;
+    
     return (
       <section className="session-form">
+        {errors && <div>You've got errors</div>}
         <form onSubmit={this.handleSubmit}>
           <h1>
-            {
-              (formType === 'login') ?
-              'Log In to Foton' :
-              'Join Foton'
-            }
+            {formType === 'login' ? 'Log In to Foton' : 'Join Foton'}
           </h1>
           <label>
             Username
             <input
+              className={
+                errors && errors.username ?
+                'input-block input-session input-invalid' :
+                'input-block input-session'
+              }
               type="text"
+              autoComplete="username"
               onChange={this.handleChange('username')}
+              autoFocus
             />
           </label>
           <label>
             Password
             <input
+              className={
+                errors && errors.password ?
+                'input-block input-session input-invalid' :
+                'input-block input-session'
+              }
               type="password"
+              autoComplete="off"
               onChange={this.handleChange('password')}
             />
           </label>
@@ -50,6 +71,20 @@ class SessionForm extends React.Component {
             type="submit"
             value={formType === 'login' ? 'Log in' : 'Sign up'}
           />
+          <button>
+            Guest Sign In
+          </button>
+          <p className="session-form-redirect">
+            {
+              (formType === 'login') ? [
+                "Don't have an account yet? ",
+                <Link key="login" to="/signup/">Sign up</Link>
+              ] : [
+                'Already have an account? ',
+                <Link key="signup" to="/login/">Log in</Link>
+              ]
+            }
+          </p>
         </form>
       </section>
     );
--- frontend/components/session_form/session_form_container.jsx
+++ frontend/components/session_form/session_form_container.jsx
@@ -1,20 +1,24 @@
 import { connect } from 'react-redux';
+import { logIn, signUp } from '../../actions/session_actions';
 import SessionForm from './session_form';
 
 const mapStateToProps = (state, ownProps) => {
-  const formType =
-    (ownProps.location.pathname === '/login') ?
-    'logIn' :
-    'signUp';
-  return { formType };
+  const formType = ownProps.match.params.formType;
+  const errors = state.errors.session;
+  return {
+    formType,
+    errors
+  };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   const processForm =
-    (ownProps.location.pathname === '/login') ?
-    'logInUser' :
-    'createUser';
-  return { processForm };
+    (ownProps.match.params.formType === 'login') ?
+    logIn :
+    signUp;
+  return {
+    processForm: user => dispatch(processForm(user)),
+  };
 };
 
 export default connect(
