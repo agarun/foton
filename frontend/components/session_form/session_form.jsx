09ca1f7401288912135cff0bedda9c0384e2be5c
agarun <19801205+agarun@users.noreply.github.com>
2017-12-25 21:41:21 -0500

    Add session form

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
 
 class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  handleChange(field) {
+    return (e) => (
+      this.setState({
+        [field]: e.currentTarget.value
+      })
+    );
+  }
+
   render() {
+    const formType = this.props.formType;
+
     return (
-      <form>
-        <h1>Hello world.</h1>
-      </form>
+      <section className="session-form">
+        <form onSubmit={this.handleSubmit}>
+          <h1>
+            {
+              (formType === 'login') ?
+              'Log In to Foton' :
+              'Join Foton'
+            }
+          </h1>
+          <label>
+            Username
+            <input
+              type="text"
+              onChange={this.handleChange('username')}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              onChange={this.handleChange('password')}
+            />
+          </label>
+          <input
+            type="submit"
+            value={formType === 'login' ? 'Log in' : 'Sign up'}
+          />
+        </form>
+      </section>
     );
   }
 }
