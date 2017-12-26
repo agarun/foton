fbdca1455d4d1c70a87ee95ec58a78bd81db115e
agarun <19801205+agarun@users.noreply.github.com>
2017-12-26 15:38:32 -0500

    Bootstrap `currentUser` to window for front-end access

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-const store = configureStore();
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      session: {
+        currentUser: window.currentUser
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
   ReactDOM.render(
     <Root store={store} />,
     document.getElementById('root')
