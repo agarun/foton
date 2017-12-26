9703a6f4205220f0c0cac576a5234ab830568eaa
agarun <19801205+agarun@users.noreply.github.com>
2017-12-26 14:27:15 -0500

    Update session reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,7 +1,11 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
 const sessionReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return action.currentUser;
     default:
       return state;
   }
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,17 +2,22 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger'; /* TODO: OMIT IN PRODUCTION */
+import { compose } from 'redux'; /* TODO: OMIT IN PRODUCTION */
 
 const middlewares = [
   thunk,
   logger /* TODO: OMIT IN PRODUCTION */
 ];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; /* TODO: OMIT IN PRODUCTION */
+
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(...middlewares)
+    composeEnhancers(
+      applyMiddleware(...middlewares)
+    )
   )
 );
 
