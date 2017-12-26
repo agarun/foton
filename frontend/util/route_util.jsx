10c6876d88104af654a92b75061abd2f74dbf04b
agarun <19801205+agarun@users.noreply.github.com>
2017-12-26 15:51:30 -0500

    Add protected and auth route utilities

---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const Auth = ({ component: Component, path, isLoggedIn }) => (
+  <Route
+    path={path}
+    render={props => (
+      isLoggedIn ? (
+        <Redirect to="/" />
+      ) : (
+        <Component {...props} />
+      )
+    )}
+  />
+);
+
+const Protected = ({ component: Component, path, isLoggedIn }) => (
+  <Route
+    path={path}
+    render={props => (
+      isLoggedIn ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    )}
+  />
+);
+
+const mapStateToProps = state => ({
+  isLoggedIn: !!state.session.currentUser
+});
+
+export const AuthRoute = withRouter(
+  connect(
+    mapStateToProps
+  )(Auth)
+);
+
+export const ProtectedRoute = withRouter(
+  connect(
+    mapStateToProps
+  )(Auth)
+);
