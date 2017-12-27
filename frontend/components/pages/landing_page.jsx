76413b287efd00513f1c7aa5c47e758b0c577eae
agarun <19801205+agarun@users.noreply.github.com>
2017-12-27 16:29:15 -0500

    Add landing page

---
+++ frontend/components/pages/landing_page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LandingPage = () => (
+  <section className="landing-image">
+    <section className="landing-center">
+      <h1>Find inspiration and showcase your work</h1>
+      <h2>A community for everyone's best photos</h2>
+      <Link
+        className="landing-button"
+        to="/signup"
+      >
+        Join Foton
+      </Link>
+  </section>
+  </section>
+);
+
+export default LandingPage;
