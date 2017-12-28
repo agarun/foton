9c072b649c3c9f7950aeb3ddc57d4516c03a24cd
agarun <19801205+agarun@users.noreply.github.com>
2017-12-28 11:40:15 -0500

    Add photo level validations on profile photo & cover photo booleans

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,12 @@
 class Photo < ApplicationRecord
+  validates :is_cover_photo,
+            uniqueness: { scope: :author_id,
+                          message: 'can only have 1 cover photo' },
+            if: :is_cover_photo
+  validates :is_profile_photo,
+            uniqueness: { scope: :author_id,
+                          message: 'can only have 1 profile photo' },
+            if: :is_profile_photo
+
+  belongs_to :author, class_name: :User
 end
