d246a6a2f2ba9de3348a36ab08ae4967f01ee5c8
agarun <19801205+agarun@users.noreply.github.com>
2017-12-29 12:27:14 -0500

    Update photo attachment validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,10 +2,13 @@ class Photo < ApplicationRecord
   belongs_to :author, class_name: :User
 
   has_attached_file :image,
-                    styles: { medium: "960x960>", thumb: "100x100>" },
-                    default_url: "/images/:style/default.png"
+                    styles: { medium: "960x960>", thumb: "100x100>" }
 
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_attachment :image,
+                       presence: true,
+                       content_type: { content_type: /\Aimage\/.*\z/ },
+                       size: { in: 0..10.megabytes }
+
   validates :is_cover_photo,
             uniqueness: { scope: :author_id,
                           message: 'error: can only have 1 cover photo' },
