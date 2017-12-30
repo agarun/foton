e83cce9b87af8d4ebeaf545bbcfd422fb9023ac1
agarun <19801205+agarun@users.noreply.github.com>
2017-12-29 23:54:54 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,136 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+  User.create!(username: "adventurer", password: "password", bio: "Everyone likes a good aventure!", location: "New York"),
+  User.create!(username: "aaron", password: "hunter2", bio: "", location: "Brooklyn"),
+  User.create!(username: "Rosalia", password: "hunter2", bio: "", location: "Barcelona"),
+  User.create!(username: "Sinjin", password: "hunter2", bio: "I love fractals.", location: "Jersey"),
+  User.create!(username: "Kamasi", password: "hunter2", bio: "Jazz is like a telescope.", location: "Los Angeles"),
+  User.create!(username: "Loyle", password: "hunter2", bio: "", location: "London"),
+  User.create!(username: "Sevda", password: "hunter2", bio: "", location: "Baku"),
+  User.create!(username: "Kacy", password: "hunter2", bio: "Musician. Send me good music!", location: "Phoenix"),
+  User.create!(username: "Jane", password: "hunter2", bio: "", location: "Israel"),
+  User.create!(username: "Olafur", password: "hunter2", bio: "The greatest thing about being a musician is being in the position to inspire other people.", location: "Iceland"),
+  User.create!(username: "Ludovico", password: "hunter2", bio: "It's only when we become aware that our time is limited that we can channel our energy into truly living.", location: "Milan"),
+  User.create!(username: "Jorja", password: "hunter2", bio: "", location: "Walsall")
+]
 
-User.create!(username: "adventurer", password: "password")
-User.create!(username: "aaron", password: "testing")
+hundred_photos = Dir.glob("*.{jpg,jpeg}")
+
+hundred_photos_attributes = [
+  ["Energy in Positano", "Positano, Italy, a village on southern Italy's Amalfi Coast"],
+  ["Sunshine"],
+  ["Bagan Temples", "Bagan, Myanmar"],
+  ["Cycle", "Aerial resort shot!"],
+  ["Vivid"],
+  ["Cerulean"],
+  ["Contrast"],
+  ["Bright Antelope", "Antelope Canyon in Arizona!"],
+  ["Aurora"],
+  ["Movement", "A sweet top-down shot of some rowers"],
+  ["Silhouette", ""],
+  ["Ice Ice"],
+  ["Crevice"],
+  ["Entrance"],
+  ["Experience"],
+  ["Crocodile Eyes", "Studies have shown that crocodile eyes are fine-tuned for lurking at the water surface to watch for prey via their \"fovea\"."],
+  ["Unique Eyes", "Girl from Basirhat, West Bengal"],
+  ["Cookies!", "Bakin' cookies."],
+  ["Warm Vibes"],
+  ["Keen Squirrel"],
+  ["Mass"],
+  ["Ants"],
+  [""],
+  ["Evergreen"],
+  ["Industrial"],
+  ["Sine"],
+  [""],
+  ["White Cliffs"],
+  ["Earth Tones"],
+  ["Winding Road"],
+  ["Cargo"],
+  ["Saturation"],
+  ["Morning"],
+  ["Rich"],
+  ["Treats and Sugar"],
+  ["Symmetry"],
+  ["Angles"],
+  [""],
+  ["City Lights"],
+  ["Dubai Skies"],
+  ["Heli Nights"],
+  [""],
+  [""],
+  [""],
+  ["Flight"],
+  ["Golden Travels"],
+  ["Pray for Prey"],
+  ["Dinner"],
+  [""],
+  ["Tradition"],
+  ["Health"],
+  ["Summit"],
+  ["Intricate"],
+  ["Antelope Ridges"],
+  [""],
+  ["Complementary Feathers"],
+  [""],
+  ["Ink Dip"],
+  ["Crimson Geometry"],
+  ["Tight"],
+  ["Desert Quads"],
+  ["Splash"],
+  ["Clear and Clean"],
+  ["Modern Cage"],
+  ["Layers and Shadows"],
+  ["Curves"],
+  ["Structure"],
+  ["Associate"],
+  ["Weave", "Messplatz, Switzerland"],
+  ["Inset"],
+  ["Spikes"],
+  ["Melange"],
+  ["Vast"],
+  ["Brushes"],
+  ["Trapezoidal"],
+  ["Neon Jellyfish"],
+  [""],
+  ["Happy"],
+  [""],
+  ["Hue"],
+  [""],
+  [""],
+  [""],
+  [""],
+  [""],
+  ["", "Greece"],
+  ["Metropolis"],
+  ["Sundown", "Timing is everything."],
+  [""],
+  ["Mirage"],
+  ["Cubes"],
+  ["Slice"],
+  ["Industry"],
+  ["Infinite"],
+  ["Roamer"],
+  ["First Light"],
+  ["Streaks"],
+  ["Drops"],
+  ["Texture"]
+]
+
+num_photos_to_test = 100
+hundred_photos_data = (0...num_photos_to_test).map do |n|
+  [hundred_photos[n]] + hundred_photos_attributes[n]
+end
+
+hundred_photos_data.each do |row|
+  photo = File.open(row.first)
+  Photo.create(
+    author_id: users[rand(users.size)].id,
+    title: row[1],
+    description: row[2]
+  )
+end
+
+# TODO: upload custom profile photos for seeded users
+# TODO: choose which ones will be cover photos for users
+# TODO: choose which ones will be landing photos for site
