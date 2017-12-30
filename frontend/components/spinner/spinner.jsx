4164e7cd4eb978f3891bde29f03151fa45d4261f
agarun <19801205+agarun@users.noreply.github.com>
2017-12-30 13:33:04 -0500

    Add CSS spinner and component

---
+++ frontend/components/spinner/spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Spinner = () => (
+  <div className="spinner-wave">
+    <div className="spinner-wave-circle"></div>
+    <div className="spinner-wave-circle"></div>
+    <div className="spinner-wave-circle"></div>
+    <div className="spinner-wave-circle"></div>
+  </div>
+);
+
+export default Spinner;
