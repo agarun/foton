ad3f3479fad846a453ca48384e2518e24fe64317
agarun <19801205+agarun@users.noreply.github.com>
2017-12-31 15:28:47 -0500

    Add users reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import photosReducer from './entities/photos_reducer';
+import usersReducer from './entities/users_reducer';
 
 const errorsReducer = combineReducers({
   photos: photosReducer,
+  users: usersReducer,
 });
 
 export default errorsReducer;
