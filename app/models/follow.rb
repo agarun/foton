fad624c19a0986f3137f761fbca0804cbd4a6cac
agarun <19801205+agarun@users.noreply.github.com>
2018-01-01 17:50:31 -0500

    Add follow model associations and validations

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,7 @@
 class Follow < ApplicationRecord
+  belongs_to :follower, class_name: :User
+  belongs_to :followed, class_name: :User
+
+  validates :follower, uniqueness: { scope: :followed }
+  validates :followed, uniqueness: { scope: :follower }
 end
