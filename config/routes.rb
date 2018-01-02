6aedcbd0d26eb367e811f1d962f5a991e965a34e
agarun <19801205+agarun@users.noreply.github.com>
2018-01-01 21:19:01 -0500

    Add nested member routes for user follow & unfollow requests

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   get '/', to: 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: :create
+    resources :users, only: :create do
+      member do
+        patch :follow
+        delete :unfollow
+      end
+    end
     resources :users, only: :show, param: :username
+
     resource :session, only: %i[create destroy]
     resources :photos, only: %i[show create update destroy]
+    resource :follows, only: %i[create destroy] # TODO
   end
 end
