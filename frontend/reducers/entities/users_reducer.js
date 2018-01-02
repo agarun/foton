58f3584ef807a8e0e671f5069c59e116bbdf99fa
agarun <19801205+agarun@users.noreply.github.com>
2018-01-02 14:36:14 -0500

    Fix users reducer with lodash deep merge

--- frontend/reducers/entities/users_reducer.js
+++ frontend/reducers/entities/users_reducer.js
@@ -4,44 +4,50 @@ import {
   RECEIVE_UNFOLLOW
 } from '../../actions/user_actions';
 
+import { RECEIVE_CURRENT_USER } from '../../actions/session_actions';
+
+import merge from 'lodash/merge'; // deep merge!
+
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
 
-  let newState = Object.assign({}, state);
+  let newState = merge({}, state);
   switch (action.type) {
     case RECEIVE_USER: {
       const newUserUsername = action.user.username;
       const newUser = { [newUserUsername]: action.user };
       return Object.assign({}, state, newUser);
     }
+    case RECEIVE_CURRENT_USER:
+      if (action.currentUser) {
+        const currentUserUsername = action.currentUser.username;
+        const currentUser = { [currentUserUsername]: action.currentUser };
+        return Object.assign({}, state, currentUser);
+      }
+      break;
     case RECEIVE_FOLLOW: {
-      // debugger
-
       const {
         follower_username, followed_username, follower_id, followed_id
       } = action.followData;
 
       newState[followed_username].follower_ids.push(follower_id);
-      // newState[follower_username].following_ids.push(followed_id);
       newState[followed_username].current_user_follows = true;
-
+      newState[follower_username].following_ids.push(followed_id);
       return newState;
     }
-    case RECEIVE_UNFOLLOW: {
-      // debugger
-
+    case RECEIVE_UNFOLLOW:
       const {
         followed_username, follower_username, follower_id, followed_id
       } = action.followData;
-      const followerIdx = newState[followed_username].follower_ids.indexOf(follower_id);
-      // const followedIdx = newState[follower_username].followed_ids.indexOf(followed_id);
+      const followerIdx =
+        newState[followed_username].follower_ids.indexOf(follower_id);
+      const followedIdx =
+        newState[follower_username].following_ids.indexOf(followed_id);
 
       newState[followed_username].follower_ids.splice(followerIdx, 1);
-      // newState[follower_username].following_ids.splice(followedIdx, 1);
       newState[followed_username].current_user_follows = false;
-
+      newState[follower_username].following_ids.splice(followedIdx, 1);
       return newState;
-    }
     default:
       return state;
   }
