05d7b9631e60b93a937ef0c38f7f41ce666b2dd1
agarun <19801205+agarun@users.noreply.github.com>
2018-01-02 12:59:12 -0500

    Add user actions for following

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,14 +1,38 @@
 import * as UserApiUtil from '../util/user_api_util';
 
 export const RECEIVE_USER = 'RECEIVE_USER';
+export const RECEIVE_FOLLOW = 'RECEIVE_FOLLOW';
+export const RECEIVE_UNFOLLOW = 'RECEIVE_UNFOLLOW';
 
 export const receiveUser = user => ({
   type: RECEIVE_USER,
   user
 });
 
+export const receiveFollow = followData => ({
+  type: RECEIVE_FOLLOW,
+  followData
+});
+
+export const receiveUnfollow = followData => ({
+  type: RECEIVE_UNFOLLOW,
+  followData
+});
+
 export const fetchUser = username => dispatch => (
   UserApiUtil
     .fetchUser(username)
     .then(user => dispatch(receiveUser(user)))
+);
+
+export const followUser = user => dispatch => (
+  UserApiUtil
+    .followUser(user)
+    .then(payload => dispatch(receiveFollow(payload)))
+);
+
+export const unfollowUser = user => dispatch => (
+  UserApiUtil
+    .unfollowUser(user)
+    .then(payload => dispatch(receiveUnfollow(payload)))
 );
