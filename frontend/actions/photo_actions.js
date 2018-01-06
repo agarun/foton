70a15deb7fde78b2845aa60bf8089e5155b93180
agarun <19801205+agarun@users.noreply.github.com>
2018-01-06 18:45:00 -0500

    Update photo actions

--- frontend/actions/photo_actions.js
+++ frontend/actions/photo_actions.js
@@ -1,11 +1,17 @@
 import * as PhotoApiUtil from '../util/photo_api_util';
 
 export const RECEIVE_PHOTO = 'RECEIVE_PHOTO';
+export const REMOVE_PHOTO = 'REMOVE_PHOTO';
 export const RECEIVE_PHOTO_FEED = 'RECEIVE_PHOTO_FEED';
 export const RECEIVE_UPLOAD_ERRORS = 'RECEIVE_UPLOAD_ERRORS';
 
 export const receivePhoto = photo => ({
   type: RECEIVE_PHOTO,
+  photo
+});
+
+export const removePhoto = photo => ({
+  type: REMOVE_PHOTO,
   photo
 });
 
@@ -39,6 +45,12 @@ export const updatePhoto = photo => dispatch => (
   PhotoApiUtil
     .updatePhoto(photo)
     .then(updatedPhoto => dispatch(receivePhoto(updatedPhoto)))
+);
+
+export const deletePhoto = photoId => dispatch => (
+  PhotoApiUtil
+    .deletePhoto(photoId)
+    .then(photo => dispatch(removePhoto(photo)))
 );
 
 export const fetchPhotoFeed = () => dispatch => (
