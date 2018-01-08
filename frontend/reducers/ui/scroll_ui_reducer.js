adf4affc43cabeeb2ea2e9b6b67c511a80825750
agarun <19801205+agarun@users.noreply.github.com>
2018-01-07 19:52:08 -0500

    Add scroll UI reducer

---
+++ frontend/reducers/ui/scroll_ui_reducer.js
@@ -0,0 +1,33 @@
+import {
+  FETCH_PHOTO_FEED_PAGE,
+  RECEIVE_PHOTO_FEED,
+} from '../../actions/photo_actions';
+
+import merge from 'lodash/merge';
+
+const initialState = {
+  feed: {
+    isFetchingNextPage: false,
+    hasNextPage: false,
+  },
+};
+
+const modalUiReducer = (state = initialState, action) => {
+  Object.freeze(state);
+
+  let newState = merge({}, state);
+  switch (action.type) {
+    case FETCH_PHOTO_FEED_PAGE:
+      newState.feed.isFetchingNextPage = true;
+      return newState;
+    case RECEIVE_PHOTO_FEED:
+      newState.feed.isFetchingNextPage = false;
+      newState.feed.hasNextPage =
+        action.feedData.photos && Boolean(Object.keys(action.feedData.photos));
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default modalUiReducer;
--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import uploadUiReducer from './ui/upload_ui_reducer';
 import modalUiReducer from './ui/modal_ui_reducer';
+import scrollUiReducer from './ui/scroll_ui_reducer';
 
 const uiReducer = combineReducers({
   upload: uploadUiReducer,
   modal: modalUiReducer,
+  scroll: scrollUiReducer,
 });
 
 export default uiReducer;
