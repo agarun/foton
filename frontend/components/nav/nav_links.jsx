9c1901c7fa3b005b1f04fbacaaa9059c4c10ed2f
agarun <19801205+agarun@users.noreply.github.com>
2018-01-10 10:32:32 -0500

    Use NavLink in navbar

--- frontend/components/nav/nav_links.jsx
+++ frontend/components/nav/nav_links.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavLinks = () => (
   <ul>
-    <li><Link to="/discover">Discover</Link></li>
-    <li><Link to="/about">About</Link></li>
+    <li>
+      <NavLink to="/discover" activeClassName="nav-link-active">
+        Discover
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/about" activeClassName="nav-link-active">
+        About
+      </NavLink>
+    </li>
   </ul>
 );
 
