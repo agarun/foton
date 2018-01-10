f919ed9913ab5b08fe744efcf49c23052cae81d0
agarun <19801205+agarun@users.noreply.github.com>
2018-01-09 22:02:54 -0500

    Add five landing page images

--- frontend/components/pages/landing_page.jsx
+++ frontend/components/pages/landing_page.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LandingPage = () => (
-  <section className="landing-image">
+  <section
+    className={`landing-image landing-image-${Math.floor(Math.random() * 5 + 1)}`}
+  >
     <section className="landing-center">
       <h1>Find inspiration and showcase your work</h1>
       <h2>A community for everyone's best photos</h2>
