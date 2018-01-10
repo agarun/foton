5bc179e2041ca6b3c081036dc5db337b7d7f1734
agarun <19801205+agarun@users.noreply.github.com>
2018-01-09 20:41:50 -0500

    Update search controller

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,25 +1,36 @@
 class Api::SearchController < ApplicationController
   def query
     if params[:type] == "photos"
-      if params[:query].empty?
-        @photos =
-          Photo.order("RANDOM()").where(is_profile_photo: false).limit(10)
-      else
-        # TODO: Check if this is this going to escape characters like %
-        @photos = Photo
-          .where(
-            "title LIKE :query OR description LIKE :query",
-            query: "%#{params[:query]}%"
-          )
-      end
+      @photos = photo_query
       render :photos
     elsif params[:type] == "users"
-      @users = User
+      @users = user_query
+      render :users
+    end
+  end
+
+  def photo_query
+    if params[:query].empty?
+      @photos =
+        Photo.order("RANDOM()").where(is_profile_photo: false).limit(10)
+    else
+      @photos = Photo
+        .where(
+          "title LIKE :query OR description LIKE :query",
+          query: "%#{params[:query]}%"
+        )
+    end
+  end
+
+  def user_query
+    if params[:query].empty?
+      User.order("RANDOM()").limit(12)
+    else
+      User
         .where(
           "username LIKE :query",
           query: "%#{params[:query]}%"
         )
-      render :users
     end
   end
 end
