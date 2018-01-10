e58395de8a16d6ae6bb61d59da60659acf20878f
agarun <19801205+agarun@users.noreply.github.com>
2018-01-10 17:11:11 -0500

    Add discover UI reducer

---
+++ frontend/reducers/ui/discover_ui_reducer.js
@@ -0,0 +1,45 @@
+import { combineReducers } from 'redux';
+import { RECEIVE_PHOTOS } from '../../actions/photo_actions';
+import { RECEIVE_USERS } from '../../actions/user_actions';
+import { RECEIVE_TAG_PHOTO_IDS } from '../../actions/discover_actions';
+import merge from 'lodash/merge';
+
+const initialState = {
+  taggedPhotoIds: [], // { tagName: [ photoIds ] }
+  userIds: [],
+};
+
+const discoverRecommendedReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  let newState = merge({}, state);
+  switch (action.type) {
+    case RECEIVE_TAG_PHOTO_IDS:
+      newState.taggedPhotoIds = action.taggedPhotoIds;
+      // debugger
+      return newState;
+    case RECEIVE_USERS:
+      newState.userIds = action.users
+        ? Object.keys(action.users)
+        : state;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+const discoverEditorsChoiceUiReducer = (state = [], action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_PHOTOS:
+      return action.photos ? Object.keys(action.photos) : state;
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  recommended: discoverRecommendedReducer,
+  editorsChoice: discoverEditorsChoiceUiReducer,
+});
--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -3,12 +3,14 @@ import uploadUiReducer from './ui/upload_ui_reducer';
 import modalUiReducer from './ui/modal_ui_reducer';
 import scrollUiReducer from './ui/scroll_ui_reducer';
 import searchUiReducer from './ui/search_ui_reducer';
+import discoverUiReducer from './ui/discover_ui_reducer';
 
 const uiReducer = combineReducers({
   upload: uploadUiReducer,
   modal: modalUiReducer,
   scroll: scrollUiReducer,
   search: searchUiReducer,
+  discover: discoverUiReducer,
 });
 
 export default uiReducer;
