16f6907070d5409f38bf3776e9b7bace949654b7
agarun <19801205+agarun@users.noreply.github.com>
2018-01-11 18:35:27 -0500

    Update photos controller

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,6 +1,6 @@
 class Api::PhotosController < ApplicationController
   before_action :require_user_logged_in,
-                only: %i[create update destroy]
+                only: %i[create update destroy like unlike]
 
   def index
     if current_user
@@ -57,6 +57,20 @@ class Api::PhotosController < ApplicationController
     render :show
   end
 
+  def like
+    photo_to_like = Photo.find(params[:id])
+    photo_to_like.like(current_user)
+    render partial: 'api/likes/like_data',
+           locals: { liker: current_user, photo: photo_to_like }
+  end
+
+  def unlike
+    photo_to_unlike = Photo.find(params[:id])
+    photo_to_unlike.unlike(current_user)
+    render partial: 'api/likes/like_data',
+           locals: { liker: current_user, photo: photo_to_unlike }
+  end
+
   private
 
   def photo_params
@@ -65,8 +79,8 @@ class Api::PhotosController < ApplicationController
 
   def build_tags(tag_data)
     tag_data.map do |tag|
-      Tag.find_by(name: tag["value"].downcase) ||
-      Tag.create!(name: tag["value"].downcase)
+      Tag.find_by(name: tag["value"]) ||
+      Tag.create!(name: tag["value"])
     end
   end
 end
