106fec2413f5bc62fc3b3d1f870eb7a05d6d1f8a
agarun <19801205+agarun@users.noreply.github.com>
2018-01-11 23:30:44 -0500

    Fix editors choice

--- app/controllers/api/discover_controller.rb
+++ app/controllers/api/discover_controller.rb
@@ -1,19 +1,34 @@
 class Api::DiscoverController < ApplicationController
   EDITORS_CHOICE_PHOTOS = [
-    "003.jpg",
-    "005.jpg",
-    "007.jpg",
-    "011.jpg",
-    "012.jpg",
-    "016.jpg",
-    "027.jpg",
-    "033.jpg",
-    "041.jpg",
-    "046.jpg",
-    "047.jpg",
-    "060.jpg",
-    "084.jpg",
+    "open-uri20180112-4-1zqk44",
+    "open-uri20180112-4-wz2itb",
+    "open-uri20180112-4-1al0mj9",
+    "open-uri20180112-4-b4tigs",
+    "open-uri20180112-4-1groftp",
+    "open-uri20180112-4-trc1pu",
+    "open-uri20180112-4-182jjfo",
+    "open-uri20180112-4-m0kzkq",
+    "open-uri20180112-4-bn8o2q",
+    "open-uri20180112-4-45blvn",
+    "open-uri20180112-4-hkf9a3",
+    "open-uri20180112-4-1gb5xhl",
+    "open-uri20180112-4-q5llwt",
   ].freeze
+  # EDITORS_CHOICE_PHOTOS = [
+  #   "003.jpg",
+  #   "005.jpg",
+  #   "007.jpg",
+  #   "011.jpg",
+  #   "012.jpg",
+  #   "016.jpg",
+  #   "027.jpg",
+  #   "033.jpg",
+  #   "041.jpg",
+  #   "046.jpg",
+  #   "047.jpg",
+  #   "060.jpg",
+  #   "084.jpg",
+  # ].freeze
 
   def recommended
     @users = popular_users
