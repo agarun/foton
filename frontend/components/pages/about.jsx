94743bdde77d856c6cace91d1dd3ae23bd12a757
agarun <19801205+agarun@users.noreply.github.com>
2018-01-12 21:03:24 -0500

    Add about page

--- frontend/components/pages/about.jsx
+++ frontend/components/pages/about.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { GlobeSVG, GitHubSVG, LinkedInSVG } from '../svg/contact';
+import LogoSVG from '../svg/logo';
 
-const About = ({}) => (
-  <section className="about"></section>
+const About = () => (
+  <section className="main">
+    <section className="about">
+      <div className="about-logotype">
+        <LogoSVG />
+      </div>
+      <div className="about-blurb">
+        Foton is a single-page application clone of 500px, a website for sharing and discovering photos.
+      </div>
+      <div className="about-logo">
+        <img src="https://i.imgur.com/qlxXw3z.png" />
+      </div>
+      <section className="about-content">
+        <div className="about-description">
+          The app uses React together with Redux on the front end and a Rails API with a PostgreSQL database on the back end. AWS S3 buckets handle photo storage.
+          <br />
+          Read more on <a href="https://github.com/agarun/foton">GitHub</a>.
+        </div>
+        <div className="about-links">
+          <a href="https://agarun.com"><GlobeSVG /></a>
+          <a href="https://github.com/agarun"><GitHubSVG /></a>
+          <a href="https://www.linkedin.com/in/agarunov/"><LinkedInSVG /></a>
+        </div>
+      </section>
+    </section>
+  </section>
 );
 
 export default About;
