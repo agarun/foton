06266195a30ca32217a7a404ed18032d7c36c124
agarun <19801205+agarun@users.noreply.github.com>
2018-01-12 22:42:12 -0500

    Update about page styles

--- frontend/components/pages/about.jsx
+++ frontend/components/pages/about.jsx
@@ -9,7 +9,7 @@ const About = () => (
         <LogoSVG />
       </div>
       <div className="about-blurb">
-        Foton is a single-page application clone of 500px, a website for sharing and discovering photos.
+        <span className="about-blurb-bold">Foton</span> is a single-page application clone of 500px, a website for sharing and discovering photos.
       </div>
       <div className="about-logo">
         <img src="https://i.imgur.com/qlxXw3z.png" />
@@ -18,7 +18,7 @@ const About = () => (
         <div className="about-description">
           The app uses React together with Redux on the front end and provides a Rails API with a PostgreSQL database in the back end. AWS S3 buckets handle photo storage.
           <br />
-          Read more on <a href="https://github.com/agarun/foton">GitHub</a>.
+          Read more on <a href="https://github.com/agarun/foton">GitHub</a> or visit the author's portfolio:
         </div>
         <div className="about-links">
           <a href="https://agarun.com"><GlobeSVG /></a>
