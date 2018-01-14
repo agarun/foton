6714e101faea5f3c55526943b0fb5b0f271aa2d1
agarun <19801205+agarun@users.noreply.github.com>
2018-01-14 15:19:03 -0500

    Remove redundant imports

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,12 +6,9 @@ import { ProtectedRoute } from '../util/route_util';
 
 import Nav from './nav/nav';
 import SessionForm from './session_form/session_form_container';
-import UserProfile from './users/user_profile_container';
 import PhotoManage from './photos/photo_manage_container';
 import PhotoSwitch from './photos/photo_show_switch';
-
 import ModalRoot from './modal/modal_root';
-import PageNotFound from './pages/404';
 
 const App = () => (
   <main>
